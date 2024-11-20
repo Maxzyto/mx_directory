53e48190825e20131f551ba7214fff3699e80a16
Maxzyto <adaaziabugrimaxwell@gmail.com>
2024-11-20 03:19:48 +0000

    typegene added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,25 +3,29 @@ import SearchForm from "@/components/SearchForm"
 import StartupCard from '@/components/StartupCard';
 import { title } from 'process';
 import { StartupTypeCard } from '@/components/StartupCard';
+import {STARTUPS_QUERY} from '@/sanity/lib/queries';
+import { client } from '@/sanity/lib/client';
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query
 
-  const posts = [
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { _Id: 1, name:"Maxwell" },
-      _id: 1,
-      description: "This is a description.",
-      image:
-        "https://www.istockphoto.com/photo/robot-assistant-vacuuming-gm1924607771-555584833?utm_campaign=srp_photos_bottom&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fwe-roboots&utm_medium=affiliate&utm_source=unsplash&utm_term=we+roboots%3A%3A%3A",
-      category: "Robots",
-      title: "We Robots",
-    },
-  ];
+  const posts = await client.fetch(STARTUPS_QUERY);
+
+  // const posts = [
+  //   {
+  //     _createdAt: new Date(),
+  //     views: 55,
+  //     author: { _Id: 1, name:"Maxwell" },
+  //     _id: 1,
+  //     description: "This is a description.",
+  //     image:
+  //       "https://www.istockphoto.com/photo/robot-assistant-vacuuming-gm1924607771-555584833?utm_campaign=srp_photos_bottom&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fwe-roboots&utm_medium=affiliate&utm_source=unsplash&utm_term=we+roboots%3A%3A%3A",
+  //     category: "Robots",
+  //     title: "We Robots",
+  //   },
+  // ];
 
   return (
     <>
@@ -43,7 +47,7 @@ export default async function Home({ searchParams }: {
           {
             posts?.length > 0 ? (
               // eslint-disable-next-line @typescript-eslint/no-unused-vars
-              posts.map((post: StartupTypeCard, index: number) => (
+              posts.map((post: StartupTypeCard) => (
                 <StartupCard key={post?._id} post={post} />
               ))
             ):(
